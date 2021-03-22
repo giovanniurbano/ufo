d67db18605d7300412ee7c8388dc49fc8a77a9f5
giovanniurbano <urbagio@gmail.com>
2021-03-22 13:02:50 +0100

    Versione con grafica base funzionante

--- src/main/java/it/polito/tdp/ufo/model/Model.java
+++ src/main/java/it/polito/tdp/ufo/model/Model.java
@@ -2,6 +2,21 @@ package it.polito.tdp.ufo.model;
 
 import java.util.List;
 
+import it.polito.tdp.db.SightingDAO;
+
 public class Model {
-	private List<String> formeUfo;
+	private List<String> formeUFO = null ;
+	
+	public List<String> getFormeUFO() {
+		if (this.formeUFO==null) {
+			SightingDAO dao = new SightingDAO() ;
+			this.formeUFO = dao.readShapes() ;
+		}
+		return this.formeUFO;
+	}
+	
+	public int getCountByForma(String forma) {
+		SightingDAO dao = new SightingDAO();
+		return dao.countByShape(forma) ;
+	}
 }
